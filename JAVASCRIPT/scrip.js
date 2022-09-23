387aeee8e01f27d96c33e91fe5448a968ffebfa1
Jean Reyes <eliasreyes1601@gmail.com>
2022-09-22 22:51:38 -0500

    Calculadora IMC

--- JAVASCRIPT/scrip.js
+++ JAVASCRIPT/scrip.js
@@ -1,1 +1,22 @@
-alert("Hola Mundo")
+window.onload = iniciar();
+
+function iniciar() {
+    var btnCalcular = document.getElementById("btnCalcular");
+    btnCalcular.addEventListener("click", clickBtnCalcular);
+}
+
+function clickBtnCalcular() {
+    var txtAltura = document.getElementById("txtAltura");
+    var altura = txtAltura.value;
+
+    var txtPeso = document.getElementById("txtPeso");
+    var peso = txtPeso.value;
+
+    var IMC = Math.round(calcularImc(peso,altura));
+    alert("Su IMC es: " + IMC);
+}
+
+function calcularImc(peso, altura) {
+    var IMC = (10**4)*(peso/(altura**2));
+    return IMC;
+}
