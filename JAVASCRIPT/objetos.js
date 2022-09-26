c0ad6f84096cd0e540ada698b8523bebfa882cdb
Jean Reyes <eliasreyes1601@gmail.com>
2022-09-25 22:40:32 -0500

    JSON STRINGIFY Y PAR, ADEMÁS DEL LOCALSTORAGE

--- JAVASCRIPT/objetos.js
+++ JAVASCRIPT/objetos.js
@@ -1,9 +1,15 @@
-window.onload = iniciar();
 var personas = [];
-
+window.onload = iniciar();
 function iniciar() {
+    personas = JSON.parse(localStorage.personas);
+    mostrarPersonas();
+    setTimeout(Bienvenido, 3000);
     var btnCrearObjeto = document.getElementById("btnCrearObjeto");
     btnCrearObjeto.addEventListener("click", clickBtnAgregar);
+}
+
+function Bienvenido(){
+    alert("Espero la estés pasando bien!!!");
 }
 
 function clickBtnAgregar() {
@@ -12,7 +18,7 @@ function clickBtnAgregar() {
     var edad = document.getElementById("txtEdad").value;
 
     crearPersona(nombre, apellido, edad);
-
+    mostrarPersonas();
 }
 
 function crearPersona(nombre, apellido, edad){
@@ -23,15 +29,17 @@ function crearPersona(nombre, apellido, edad){
         "Apellido"  : apellido,
         "Edad"      : edad
     }
-    personas.push(persona)
+    personas.push(persona);
+    mostrarPersonas();
+    localStorage.personas = JSON.stringify(personas);
+}
 
+function mostrarPersonas() {
     var html = "";
 
     for (var persona of personas) {
         html += "<tr><td>"+persona.ID+"</td><td>"+persona.Nombre+"</td><td>"+persona.Apellido+"</td><td>"+persona.Edad+"</td></tr>";
     }
-
-
     var body = document.getElementById("tBody");
     body.innerHTML = html;
 }
